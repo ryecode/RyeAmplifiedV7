3d05fbe9d39ed72426ddccbd8f975b882378f645
Rye <mr.ryecorral@gmail.com>
2024-02-29 10:45:31 +0800

    Added common components, Routing, and Auth pages

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
+import { Route, Routes } from 'react-router-dom';
 
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import SiteNav from './components/Common/SiteNav';
+import SiteFooter from './components/Common/SiteFooter';
+import HomePage from './components/home/HomePage';
+import LoginPage from './auth/LoginPage';
+import RegisterPage from './auth/RegisterPage';
+
 
 function App() {
   return (
     <div >
-      <header >
-
-      </header>
+      <SiteNav />
+      <Routes>
+        <Route path="/" exact={true} element={<HomePage />} />
+        <Route path="*" element={<HomePage />} />
+        <Route path='/login' element={<LoginPage />} />
+        <Route path='/register' element={<RegisterPage />} />
+      </Routes>
+      <SiteFooter />
     </div>
   );
 }
---
+++ src/components/home/HomePage.js
@@ -0,0 +1,26 @@
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Image from "react-bootstrap/Image";
+import Button from "react-bootstrap/Button";
+
+
+
+function HomePage() {
+  return (
+    <Container>
+        <Row className="px-4 my-5">
+            <Col xs={4} sm={6}>   
+                <Image src="/img/RYELogoBlackSphere.png" fluid /> 
+            </Col>
+            <Col sm={6}>   
+                <h1 className="font-weight-light">RYE Amplified™ Technologies</h1>
+                <p>Amplifying the voices of the Rye community</ p>
+                <Button variant="primary">Learn More</Button>   
+            </Col>
+        </Row>
+    </Container>
+  );
+}
+
+export default HomePage;   
